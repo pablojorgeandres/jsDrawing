4d53172a65852e621a4567b72b88bb622436b7f3
Pablo Andres <pablo@Pablos-MacBook-Pro.local>
2018-12-23 20:17:59 -0300

    Empezando a dar estilos y agregar variables.

--- functions.js
+++ functions.js
@@ -1,38 +1,39 @@
-document.addEventListener("mousemove", detectWhereIsMouse);
+// document.addEventListener("mousemove", detectWhereIsMouse);
+document.addEventListener("mousemove", startTrace);
 
 var canvasObj = document.getElementById('area_de_dibujo');
 var canvas = canvasObj.getContext("2d");
 var canvasOffSetX = canvasObj.offsetLeft;
 var canvasOffSetY = canvasObj.offsetTop;
 var color = 'black';
-
-
-function detectWhereIsMouse(event) {
-    var point = getCoords(event);
-    if (point.x > 0 && point.x < 501 && point.y > 0 && point.y < 501) {
-        document.addEventListener("mousemove", startTrace);
-        document.addEventListener("mousemove", startTrace);
-        document.addEventListener("mousemove", startTrace);
-        document.addEventListener("mousemove", startTrace);
-    }
-}
+var x,y;
 
 function startTrace() {
+    
     var mouseDown = false;
-    document.addEventListener('mousedown', function() { mouseDown = true })
+    var startPoint = getCoords(event);
+    document.addEventListener(
+            'mousedown', 
+            function() { mouseDown = true;
+                if(startPoint.x > 0 && startPoint.x < 500 && startPoint.y > 0 && startPoint.y < 500){
+                    x = startPoint.x; 
+                    y = startPoint.y;    
+                }
+             })
     document.addEventListener('mouseup', function() { mouseDown = false })
-    document.addEventListener('mousemove', function() { 
+    document.addEventListener('mousemove', function() {
         if (!mouseDown) {
-            canvas.closePath();
             return;
-        } else {
-            var startPoint = getCoords(event);        
+        } else if( x > 0 && x < 500 && y > 0 && y < 500) {
             canvas.strokeStyle = color;
-            canvas.lineWidth = 2;
+            canvas.lineWidth = 1;
             canvas.beginPath();
-            canvas.moveTo(startPoint.x, startPoint.y);
-            canvas.lineTo(startPoint.x - 2, startPoint.y);
-            canvas.stroke();        
+            canvas.moveTo( x , y );
+            var endPoint = getCoords(event);        
+            canvas.lineTo(endPoint.x, endPoint.y);
+            canvas.stroke();
+            x = endPoint.x;
+            y = endPoint.y;
         }
     }); 
 }
